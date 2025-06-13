2fb95b09786ccc3f7abab11056d715aae87e03de
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-06-13 11:10:24 -0500

    created redux store and implementing it in other components

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Counter from './components/Counter';
+import Header from './components/Header'
+import Auth from './components/Auth'
+import UserProfile from './components/UserProfile'
+import { useSelector } from 'react-redux';
+
+function App() {
+  const isAuth = useSelector(state=>state.auth.isAuthenticated);
+
+  return (
+    <> 
+    <Header/>
+    {!isAuth && <Auth/>}
+    {isAuth && <UserProfile/>}
+    <UserProfile/>
+    <Counter />
+    </>
+   
+  );
+}
+
+export default App;
---
+++ src/components/Auth.js
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
+import classes from './Auth.module.css';
+import { authActions } from './store';
+
+const Auth = () => {
+  const auth=useSelector(state=>state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+
+  const handleLogin=()=>{
+    dispatch(authActions.login())
+  }
+  const handleLogout=()=>{
+    dispatch(authActions.logout())
+  }
+
+  return (
+    <main className={classes.auth}>
+      <section>
+        <form>
+          <div className={classes.control}>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' />
+          </div>
+          <button onClick={handleLogin}>Login</button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default Auth;
