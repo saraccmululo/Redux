4d40735eff34f24dea9dea7bf33aed302e4a8bd6
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-06-16 13:25:34 -0500

    adding notification state to redux reducer fn

--- Advanced Redux/src/App.js
+++ Advanced Redux/src/App.js
@@ -1,21 +1,75 @@
-import { useEffect } from 'react';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
-  const showCart=useSelector(state=>state.ui.cartIsVisible);
-  const cart=useSelector((state)=>state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
 
-  useEffect(()=>{
-    fetch('https://learning-redux-react-default-rtdb.firebaseio.com/cart.json', {method: 'PUT', body: JSON.stringify(cart),})
-  },[cart])
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+
+      const response = await fetch(
+        "https://learning-redux-react-default-rtdb.firebaseio.com/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to send cart data");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success...",
+          message: "Cart data sent successfully!",
+        })
+      );
+    };
+
+    if (isInitial) {//only stops the side effect, not the app rendering. It affects whether sendCartData() runs inside useEffect, not the UI. useEffect runs after component is mounted.
+      isInitial = false;
+      return;
+    }
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Failed to send cart data!",
+        })
+      );
+    });
+  }, [cart]);
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- Advanced Redux/src/store/ui-slice.js
+++ Advanced Redux/src/store/ui-slice.js
@@ -1,14 +1,21 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const uiSlice=createSlice({
-  name:'ui',
-  initialState:{cartIsVisible: false},
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: { cartIsVisible: false, notification: null },
   reducers: {
     toggleCart(state) {
-      state.cartIsVisible=!state.cartIsVisible;
-    }
-  }
-})
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
 
-export const uiActions=uiSlice.actions;
+export const uiActions = uiSlice.actions;
 export default uiSlice;
